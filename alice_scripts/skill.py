cebf752fc05c3e89f37000ae33fa996c1c704dc2
Alexander Borzunov <hxrussia@gmail.com>
2018-07-31 19:00:40 +0500

    Parse request JSON only on POST /

--- alice_scripts/skill.py
+++ alice_scripts/skill.py
@@ -3,7 +3,7 @@ import threading
 
 import flask
 
-from .requests import Request, request
+from .requests import Request
 
 
 __all__ = ['Skill']
@@ -16,19 +16,16 @@ class Skill(flask.Flask):
         self._sessions = {}
         self._session_lock = threading.RLock()
 
-        @self.before_request
-        def save_request():
-            flask.g.request = Request(flask.request.json)
-
     def script(self, generator):
         @self.route("/", methods=['POST'])
         def handle_post():
-            logging.debug('Request: %r', request)
+            flask.g.request = Request(flask.request.get_json())
+            logging.debug('Request: %r', flask.g.request)
 
             content = self._switch_state(generator)
             response = {
-                'version': request['version'],
-                'session': request['session'],
+                'version': flask.g.request['version'],
+                'session': flask.g.request['session'],
                 'response': content,
             }
 
@@ -38,7 +35,7 @@ class Skill(flask.Flask):
         return generator
 
     def _switch_state(self, generator):
-        session_id = request['session']['session_id']
+        session_id = flask.g.request['session']['session_id']
         with self._session_lock:
             if session_id not in self._sessions:
                 state = self._sessions[session_id] = generator()
